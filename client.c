694f855acf42372b61dba2d014a10ab04ef8e274
AbheekSingh08 <130622441+AbheekSingh08@users.noreply.github.com>
2023-04-29 23:40:22 -0400

    Update client.c

--- client.c
+++ client.c
@@ -5,17 +5,21 @@
 #include <arpa/inet.h>
 #include <sys/socket.h>
 
+
 #define PORT 8080
+
 
 void prepare_server_address(struct sockaddr_in *server_addr) {
     sa_family_t family = AF_INET;
     in_addr_t addr = INADDR_ANY;
     in_port_t port = htons(PORT);
 
+
     server_addr->sin_family = family;
     server_addr->sin_addr.s_addr = addr;
     server_addr->sin_port = port;
 }
+
 
 int get_input_from_user() {
     int number;
@@ -24,22 +28,28 @@ int get_input_from_user() {
     return number;
 }
 
+
 int main() {
     int clmn;
     char bff[1024] = {0};
     int number;
     struct sockaddr_in seradd;
 
+
     clmn = socket(AF_INET, SOCK_STREAM, 0);
 
+
     prepare_server_address(&seradd);
+
 
     struct sockaddr *server_sockaddr = (struct sockaddr *)&seradd;
     socklen_t selen = sizeof(seradd);
     connect(clmn, server_sockaddr, selen);
 
+
     number = get_input_from_user();
     snprintf(bff, sizeof(bff), "%d", number);
+
 
     int sockfd = clmn;
     void *buf = bff;
@@ -47,7 +57,9 @@ int main() {
     int flags = 0;
     send(sockfd, buf, len, flags);
 
+
     close(clmn);
+
 
     return 0;
 }
