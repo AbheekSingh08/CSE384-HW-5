800b2bef206a102bd3d3191e2d982d6727e4e30d
AbheekSingh08 <130622441+AbheekSingh08@users.noreply.github.com>
2023-04-29 23:41:07 -0400

    Update server.c

--- server.c
+++ server.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
+#include <pthread.h>
 
 #define PORT 8080
 
@@ -13,15 +14,33 @@ void prepare_server_address(struct sockaddr_in *server_addr) {
     server_addr->sin_port = htons(PORT);
 }
 
+typedef struct {
+    int clfd;
+    int index;
+} cldata;
+
+int numbers[2] = {0};
+
+void *clthr(void *arg) {
+    cldata *data = (cldata *)arg;
+    int valread;
+    char buff[1024] = {0};
+    valread = read(data->clfd, buff, 1024);
+    numbers[data->index] = atoi(buff);
+    close(data->clfd);
+    free(arg);
+    return NULL;
+}
+
 int sum(int x, int y) {
-    return x + y ;
+    return x + y;
 }
 
 int main() {
     int srvfd, clfd, adlen, valread;
     struct sockaddr_in server_addr, cladd;
     char buff[1024] = {0};
-    int numbers[2] = {0};
+    int count = 0;
     srvfd = socket(AF_INET, SOCK_STREAM, 0);
 
     int opt = 1;
@@ -40,19 +59,26 @@ int main() {
     listen(srvfd, 2);
 
     adlen = sizeof(cladd);
-    int count = 0;
+
+    pthread_t threads[2];
+
     while (count < 2) {
         struct sockaddr *cltsor = (struct sockaddr *)&cladd;
         socklen_t *clptr = (socklen_t *)&adlen;
         clfd = accept(srvfd, cltsor, clptr);
 
-        valread = read(clfd, buff, 1024);
-        numbers[count] = atoi(buff);
-        close(clfd);
+        cldata *data = (cldata *)malloc(sizeof(cldata));
+        data->clfd = clfd;
+        data->index = count;
+
+        pthread_create(&threads[count], NULL, clthr, data);
         count = count + 1;
     }
 
+    pthread_join(threads[0], NULL);
+    pthread_join(threads[1], NULL);
+
     printf("Sum of two numbers: %d\n", sum(numbers[0], numbers[1]));
-//
+
     return 0;
 }
